38288a2bc0b35bc68f73bdfe38db30b565a87a1c
SWP_2024_cWI <>
2024-10-07 16:15:27 +0200

    ObserverP2

--- Observer/src/Main.java
+++ Observer/src/Main.java
@@ -1,6 +1,6 @@
 import at.htldornbirn._4cWI2024.swp.observer.NewsAgency;
 import at.htldornbirn._4cWI2024.swp.observer.Newschannel;
-s//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
