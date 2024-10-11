fb2916e042fd435b9c8394dc4ee57f385b117f36
SWP_2024_cWI <>
2024-10-11 09:58:50 +0200

    fix

---
+++ Oberserver_Part2/src/com/example/newsbroker/NewsAgency.java
@@ -0,0 +1,39 @@
+package com.example.newsbroker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsAgency {
+
+    private final String name;
+    private final List<NewsChannel> channels;
+
+    public NewsAgency(String name) {
+        this.name = name;
+        this.channels = new ArrayList<NewsChannel>();
+    }
+
+    public void subscribe(NewsChannel channel) {
+        System.out.println(channel + " has subscribed to" + this);
+        this.channels.add(channel);
+    }
+
+    public void unsubscribe(NewsChannel channel) {
+        System.out.println(channel + " has unsubscribed to" + this);
+        this.channels.remove(channel);
+    }
+
+    public void broadcast(String news) {
+        System.out.println(this + " broadcasts " + news);
+        for (NewsChannel channel : channels) {
+            channel.notify(news);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+
+}
